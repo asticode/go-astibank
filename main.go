9d1f10f688032474392681f2594d04c4d7596881
Quentin Renard <contact@asticode.com>
2017-06-03 10:28:33 +0200

    Updated window dimensions

--- main.go
+++ main.go
@@ -50,8 +50,8 @@ func main() {
 		WindowOptions: &astilectron.WindowOptions{
 			BackgroundColor: astilectron.PtrStr("#333"),
 			Center:          astilectron.PtrBool(true),
-			Height:          astilectron.PtrInt(800),
-			Width:           astilectron.PtrInt(800),
+			Height:          astilectron.PtrInt(720),
+			Width:           astilectron.PtrInt(1280),
 		},
 	}); err != nil {
 		astilog.Fatal(errors.Wrap(err, "running bootstrap failed"))
